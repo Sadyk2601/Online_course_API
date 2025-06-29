cf6e7a43bf40405654f8d7f6d4428eb07935ce09
Sadyk2601 <you@example.com>
2025-06-29 10:16:01 +0500

    finish3

--- src/results/result.module.ts
+++ src/results/result.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResultsController } from './result.controller';
 import { ResultsService } from './result.service';
 import { Assignment } from '../assignment/entity/assignment.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Assignment])],
+  imports: [TypeOrmModule.forFeature([Assignment]), AuthModule],
   providers: [ResultsService],
   controllers: [ResultsController],
 })
